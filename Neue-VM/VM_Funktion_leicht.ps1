628882bf6ab22894e5ca63d18074f57d9e1c9a7d
Jehovah28 <meissner_stefan@gmx.net>
2016-01-20 21:16:01 +0100

    Fehler korregiert

--- Neue-VM/VM_Funktion_leicht.ps1
+++ Neue-VM/VM_Funktion_leicht.ps1
@@ -9,10 +9,7 @@ param (
 [ValidateSet(1,2)]
 $VMGeneration= 2,
 
-[parameter(Mandatory)]
-# uint ist für eine Gen 2 VM erforderlich
-# für die Verwendung als Parameter ist die Angabe optional
-[uint64]$VMMemory=1GB
+$VMMemory=1GB
 )
 
 $VMLocation = 'D:\VMs'
