ed35a6c1b1f42b143270ffd283d7640ccd8856a8
Jehovah28 <meissner_stefan@gmx.net>
2016-01-20 18:58:56 +0100

    Simple function for creating a new vm

---
+++ Neue-VM/VM_Funktion_leicht.ps1
@@ -0,0 +1,33 @@
+﻿function Neue-VM {
+[cmdletbinding()] 
+
+param (
+
+[parameter(Mandatory,HelpMessage='Bitte den VM-Namen angeben')]
+[string]$VMName,
+
+[ValidateSet(1,2)]
+$VMGeneration= 2,
+
+# uint ist für eine Gen 2 VM erforderlich
+[uint64]$VMMemory
+)
+
+$VMLocation = 'D:\VMs'
+$VMNetwork = 'Lab'
+
+# VM erstellen
+$VMDiskSize = 300GB
+if ($VMMemory -ne $null)
+    {
+    New-VM -Name $VMName -Generation $VMGeneration -MemoryStartupBytes $VMMemory -SwitchName $VMNetwork -Path $VMLocation -NoVHD -Verbose
+    New-VHD -Path "$VMLocation\$VMName\Virtual Hard Disks\$VMName-Disk1.vhdx" -SizeBytes $VMDiskSize -Verbose
+    Add-VMHardDiskDrive -VMName $VMName -Path "$VMLocation\$VMName\Virtual Hard Disks\$VMName-Disk1.vhdx" -Verbose
+    }
+else 
+    {
+    New-VM -Name $VMName -Generation $VMGeneration -SwitchName $VMNetwork -Path $VMLocation -NoVHD -Verbose
+    New-VHD -Path "$VMLocation\$VMName\Virtual Hard Disks\$VMName-Disk1.vhdx" -SizeBytes $VMDiskSize -Verbose
+    Add-VMHardDiskDrive -VMName $VMName -Path "$VMLocation\$VMName\Virtual Hard Disks\$VMName-Disk1.vhdx" -Verbose
+    }
+}
